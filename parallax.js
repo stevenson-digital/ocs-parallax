33d923fa754722c60e41655e80a0c627085978d0
Ollie Stevenson <ollie@stevenson.digital>
2020-05-11 21:28:33 +0100

    [change] all jQuery removed + docs updated

--- parallax.js
+++ parallax.js
@@ -1,8 +1,5 @@
 export { parallax }
 
-let $ = jQuery.noConflict()
-const $window = $(window)
-
 const parallax = {
   isInExtendedViewport($el) {
     const elTop = $el.offsetTop
@@ -20,44 +17,21 @@ const parallax = {
     if (parallaxElements.length < 1)
       return
 
-    parallaxElements.forEach(($el, index) => {
+    parallaxElements.forEach($el => {
       if (window.innerWidth > 1024 && parallax.isInExtendedViewport($el)) {
         // Create parallax effect
-        // const speed = ($el.attr('data-speed')) ? $el.attr('data-speed') : 10
-        // const st = currentScrollY
-        // const elTop = $el.offset().top
-        // const amountScrolled = st - elTop
-        // const offset = ($el.attr('data-offset')) ? $el.attr('data-offset') : 0
-        // const scroll = ($el.attr('data-is-top')) ? currentScrollY : amountScrolled
-        // const movement = ($el.attr('data-direction') === 'down') ? (scroll / speed) - offset : 1 - (scroll / speed) - offset 
+        const speed = ($el.getAttribute('data-speed')) ? $el.getAttribute('data-speed') : 10
+        const elTop = $el.offsetTop
+        const amountScrolled = currentScrollY - elTop
+        const offset = ($el.getAttribute('data-offset')) ? $el.getAttribute('data-offset') : 0
+        const movement = ($el.getAttribute('data-direction') === 'down') ? (amountScrolled / speed) - offset : 1 - (amountScrolled / speed) - offset 
 
-        // $el.css('transform', `translate3d(0px, ${Math.round(movement)}px, 0)`)
+        $el.style.transform = `translate3d(0px, ${Math.round(movement)}px, 0)`
 
       } else {
         // Reset elements postions 
-        // $el.css('transform', 'translate3d(0px, 0px, 0')
+        $el.style.transform = 'translate3d(0px, 0px, 0'
       }
     })
-      
-    // parallaxElements.each(function(i, element) {
-    //   const $el = $(element)
-
-    //   if ($window.width() > 1024 && parallax.isInExtendedViewport($el)) {
-    //     // Create parallax effect
-    //     const speed = ($el.attr('data-speed')) ? $el.attr('data-speed') : 10
-    //     const st = currentScrollY
-    //     const elTop = $el.offset().top
-    //     const amountScrolled = st - elTop
-    //     const offset = ($el.attr('data-offset')) ? $el.attr('data-offset') : 0
-    //     const scroll = ($el.attr('data-is-top')) ? currentScrollY : amountScrolled
-    //     const movement = ($el.attr('data-direction') === 'down') ? (scroll / speed) - offset : 1 - (scroll / speed) - offset 
-
-    //     $el.css('transform', `translate3d(0px, ${Math.round(movement)}px, 0)`)
-
-    //   } else {
-    //     // Reset elements postions 
-    //     $el.css('transform', 'translate3d(0px, 0px, 0')
-    //   }
-    // })
   }
 }
